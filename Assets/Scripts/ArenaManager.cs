0c1fe0009094982b249e726e711afb487754f766
netblitzer <thenetblitzer@gmail.com>
2018-04-07 13:45:04 -0400

    First pass on units.

---
+++ Assets/Scripts/ArenaManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour {
+
+    List<Unit> Player1Units;
+
+	// Use this for initialization
+	void Start () {
+        this.Player1Units = new List<Unit>();
+        this.Player1Units.AddRange(FindObjectsOfType<Unit>());
+
+        foreach(Unit u in this.Player1Units) {
+            u.SetPosition(new Vector3(Random.Range(-10f, 10f), 0.5f, Random.Range(-10f, 10f)));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour {
+
+    public Vector3 position;
+
+    public Vector3 velocity;
+
+    public Vector3 force;
+
+    public float maxSpeed = 10f;
+
+    public float distFromSelected = 2f;
+
+    NavMeshAgent agent;
+
+    public Vector3 targetPosition;
+
+    public bool atTarget;
+
+	// Use this for initialization
+	void Start () {
+        this.agent = GetComponent<NavMeshAgent>();
+
+        this.position = this.transform.position;
+
+        this.velocity = Vector3.zero;
+        this.force = Vector3.zero;
+
+        this.atTarget = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mouseToWorld;
+            if (Physics.Raycast(mouseRay, out mouseToWorld, 1000f)) {
+                Debug.Log(mouseToWorld.point);
+                this.targetPosition = mouseToWorld.point;
+                this.atTarget = false;
+            }
+        }
+
+        if (!this.atTarget) {
+
+            Vector3 diff = this.targetPosition - this.position;
+            diff.y = 0f;
+
+            float dist = Vector3.SqrMagnitude(diff);
+
+            if (dist < this.distFromSelected) {
+                this.atTarget = true;
+            }
+            else {
+                this.ApplyForce(diff.normalized * 20f);
+            }
+        }
+        else {
+            if (this.velocity.sqrMagnitude > 1) {
+                this.ApplyForce(-this.velocity * 5f);
+            } else
+                this.velocity = Vector3.zero;
+        }
+
+        this.UpdatePhysics();
+	}
+
+    public void SetPosition (Vector3 _position) {
+        this.position = _position;
+        this.transform.position = this.position;
+    }
+
+    public void ApplyForce (Vector3 force) {
+        this.force += force;
+    }
+
+    void UpdatePhysics () {
+
+        this.velocity += this.force * Time.deltaTime;
+
+        this.force = Vector3.zero;
+
+        this.velocity = Vector3.ClampMagnitude(this.velocity, this.maxSpeed);
+
+        Vector3 targetPos = this.position + this.velocity * Time.deltaTime;
+
+        NavMeshHit posSample;
+
+        if (NavMesh.SamplePosition(targetPos, out posSample, 100f, NavMesh.AllAreas)) {
+            this.position.Set(posSample.position.x, this.position.y, posSample.position.z);
+        }
+
+        this.transform.position = this.position;
+    }
+}
